b50ad554404f3130866c2f4cc3e10fc85085ac0d
Josh Morgan <eston1007@gmail.com>
2022-12-07 02:06:43 -0800

    created script to map hero id to localized hero name

---
+++ prediction_model/strip_heroes_json.py
@@ -0,0 +1,13 @@
+import requests
+import json
+response = requests.get("https://raw.githubusercontent.com/odota/dotaconstants/master/build/heroes.json")
+
+hero_lookup = {}
+
+for hero_id, details in response.json().items():
+    hero_lookup[hero_id] = details['localized_name']
+
+print(hero_lookup)
+
+with open('prediction_model/heroes.json', 'w') as output_file:
+    json.dump(hero_lookup, output_file, indent=4)
